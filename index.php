94f9c868c0d450c480fce48725413780068ff7f5
nuelcyoung <33932707+nuelcyoung@users.noreply.github.com>
2021-04-30 13:11:52 +0100

    change

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Login</title>
-</head>
-<body>
-<h1>Login</h1>
-<form action="process.php">
-<input type="text" name="user_name" placeholder="Username">
-<input type="password" name="password" placeholder="Password">
-<input type="submit" name="login">
-</form>
-</body>
-</html>
+<?php
+$user_name = $_POST['user_name'];
+$password = htmlspecialchar($_POST['password']);
+
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -21,7 +12,7 @@
     <meta name="author" content="">
     <!--<link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">-->
 
-    <title>Starter Template for Bootstrap</title>
+    <title>ZURI TASK PHP CRUD</title>
     <!-- Bootstrap core CSS -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
@@ -32,41 +23,61 @@
 
 
   <body>
-
-    <nav class="navbar navbar-expand-md navbar-inverse fixed-top">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="register.php">Register</a>
-          </li>
-          
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
-
-    <main role="main" class="container">
-
+  <div class="navbar navbar-default navbar-static-top navbar-dark bg-dark" role="navigation">
+    <div class="container-fluid">
+ 
+        <div class="navbar-header">
+            <!-- to enable navigation dropdown when viewed in mobile device -->
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+ 
+            <!-- Change "Your Site" to your site name -->
+            <a class="navbar-brand" href="index.php">Ekenny Zuri Task</a>
+        </div>
+ 
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <!-- link to the "Cart" page, highlight if current page is cart.php -->
+                <li >
+                    <a href="index.php">Home</a>
+                </li>
+            </ul>
+ 
+                <ul class="nav navbar-nav navbar-right">
+        <li class='active'>
+            <a href="login.php">
+                <span class="glyphicon glyphicon-log-in"></span> Log In
+            </a>
+        </li>
+     
+        <li >
+            <a href="register.php">
+                <span class="glyphicon glyphicon-check"></span> Register
+            </a>
+        </li>
+      
+    </ul>
+                 
+        </div><!--/.nav-collapse -->
+ 
+    </div>
+</div>
+<div class="container">
+    <!--<main role="main" class="container">-->
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
             <form>
                 <div class="form-group">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <input type="email" name="user_name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
                 <div class="form-group form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
@@ -74,9 +85,10 @@
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
+            </div>
 
-    </main><!-- /.container -->
-
+    <!--</main> /.container -->
+</div>
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
