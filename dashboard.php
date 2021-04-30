a860b2516265786a50d965f4d4b6950b7b824f3b
nuelcyoung <33932707+nuelcyoung@users.noreply.github.com>
2021-04-30 17:19:34 +0100

    .

---
+++ dashboard.php
@@ -0,0 +1,12 @@
+<?php
+require_once("connection.php");
+require_once("layout/header.php");
+?>
+<div class="container">
+<br/>
+<br/>
+<h1>Welcome</h1>
+        <p class="lead">you can perform CRUD Operations here</p>
+</div>
+<?php require_once("layout/footer.php");?>
+</body>
